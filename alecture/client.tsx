b7624df2654e686756c00b07c107d50400e7e080
JwaSangHeon <whk4051@naver.com>
2022-08-17 14:13:02 +0900

    router 설정

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './layouts/App';
+import App from '@layouts/App';
+import { BrowserRouter } from 'react-router-dom';
 
-render(<App />, document.querySelector('#app'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
+
+// 폴더 구조
+
+// pages - 각각의 서비스 페이지
+// components - 재사용하는 컴포넌트
+// layouts - 공통 레이아웃
