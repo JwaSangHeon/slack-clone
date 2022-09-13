e1283b7e6e17d6a0f2a6936def1ae8571a8eec0e
JwaSangHeon <whk4051@naver.com>
2022-09-13 17:14:36 +0900

    workspace 제작

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -1,7 +1,20 @@
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import React, { FC, useCallback } from 'react';
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  LogOutButton,
+  WorkspaceName,
+  Workspaces,
+  Channels,
+  Chats,
+  MenuScroll,
+} from '@layouts/Workspace/styles';
 import { Redirect } from 'react-router';
+import gravatar from 'gravatar';
 import useSWR from 'swr';
 
 const Workspace: FC = ({ children }) => {
@@ -22,8 +35,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
-      {children}
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
     </div>
   );
 };
