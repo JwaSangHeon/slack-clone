3a6bbdfe66ee091ba695768cc25b09379b546543
JwaSangHeon <whk4051@naver.com>
2022-12-05 16:51:41 +0900

    chat만들기

--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,8 +1,20 @@
 import { ChatZone } from '@components/ChannelList/styles';
 import React from 'react';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
 
-const ChatList = () => {
-  return <ChatZone>section</ChatZone>;
+interface Props {
+  chatData?: IDM[];
+}
+
+const ChatList = ({ chatData }: Props) => {
+  return (
+    <ChatZone>
+      {chatData?.map((chat) => (
+        <Chat key={chat.id} data={chat} />
+      ))}
+    </ChatZone>
+  );
 };
 
 export default ChatList;
