5a5068fcf69987e12b7dcb905fb39342ec8509d3
JwaSangHeon <whk4051@naver.com>
2022-12-07 10:47:00 +0900

    날짜 처리

--- alecture/components/Chat/index.tsx
+++ alecture/components/Chat/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
@@ -17,21 +17,25 @@ const Chat = ({ data }: Props) => {
 
   //@[222](6)
   // \d 숫자 +는 1개 이상, ?는 0개나 1개, *는 0개 이상
-  const result = regexifyString({
-    input: data.content,
-    pattern: /@\[(.+?)\]\((\d+)\)|\n/g,
-    decorator(match, index) {
-      const arr: string[] | null = match.match(/@\[(.+?)\]\((\d+)\)/);
-      if (arr) {
-        return (
-          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
-            @{arr[1]}
-          </Link>
-        );
-      }
-      return <br key={index} />;
-    },
-  });
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)\]\((\d+)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)\]\((\d+)\)/);
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -48,4 +52,4 @@ const Chat = ({ data }: Props) => {
   );
 };
 
-export default Chat;
+export default memo(Chat);
